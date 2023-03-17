b9f922cf589d164f1b4af9b8e4c5cbd524be5d57
Muryllo Vieira <muryllovieira59@gmail.com>
2023-03-17 13:04:50 -0300

    API

---
+++ js/postmon.js
@@ -0,0 +1,14 @@
+'use strict'
+
+export const pesquisarCep = async (cepPesquisa) => {
+    const url = `https://api.postmon.com.br/v1/cep/${cepPesquisa}`
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return {
+        logradouro: data.logradouro,
+        bairro: data.bairro,
+        municipio: data.cidade,
+        estado: data.estado
+    }
+}
